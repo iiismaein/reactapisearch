d22f39c00cc9027f812f10250d515c3631fa30dd
iiismaein <ismaein@hotmail.com>
2022-04-25 14:14:10 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {Fragment, useEffect, useState} from "react";
+import './App.css'
+import Card from "./Card";
+import axios from 'axios'
 
 function App() {
+    const [characters,setCharacters]=useState([])
+    const [query,setQuery]=useState('')
+    const setQueryState=(event)=>{
+        setQuery(event.target.value);
+    }
+    useEffect(()=>{
+        const fetchData=async ()=>{
+            try{
+                const {data}= await axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`)
+                setCharacters(data.results)
+
+            }catch (error){
+                console.log(error)
+            }
+        }
+        fetchData()
+    },[query])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Fragment >
+          <div className={'App'}>
+              <div className={'search'}>
+        <input type={'text'} placeholder={'search'}
+               className={'input'} onChange={setQueryState}
+               value={query}
+        />
     </div>
+
+<div className="wrapper"></div>
+    <div className={'test'}>
+          {characters.map(character =>(
+              <div  key={Math.random()}>
+                  <Card >
+                      <h1>{character.id}
+                          <span>-</span>
+                          {character.name}
+                      </h1>
+                      <img src={character.image} alt={character.name}/>
+                      <p>{character.status}</p>
+                      <p>{character.species}</p>
+                      <p>{character.type}</p>
+                      <p>{character.gender}</p>
+                  </Card>
+              </div>
+          ))}
+          </div></div>
+      </Fragment>
+
   );
 }
 
